2e625adf828cad43ebf3db5f2c8be18e594867e9
maria-luiza-md <marialuizadamacesno55@gmail.com>
2026-05-22 11:52:29 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+#----------------------------SIDEBAR
+
+st.sidebar.title("Multiuso(carros)")
+
+carros = ["Jeep" ,"Dodge ram","BMW", "Mustang"]
+carro = st.sidebar.selectbox("escolha seu carros:" , carros)
+
+#-----------------------------BODY
+
+st.title("Multiuso(carros)")
+st.image(f"{carro}.png")
+st.write(f"voce alugou o {carro}")
+
+dias = st.text_input(f"por quantos dias vcoe alugou o {carro}? ")
+km = st.text_input(f"quntos km voce rodou com o {carro}?")
+
+
+if carro == "BMW":
+    diaria = 600
+
+elif carro == "Jeep":
+    diaria = 600
+
+elif carro == "Dodge ram":
+    diaria = 1.500
+
+elif carro == "Mustang":
+    diario = 2000.00
+
+if st.button("Calcular"):
+    dias = int(dias)
+    km = int (km)
+
+    total = (dias * diaria) + (km * 0.15 )
+
+    st.Waring(f"o todal do aluguel a se pegar e R${total}.SS")
+
